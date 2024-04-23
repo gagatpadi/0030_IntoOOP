f3358e0bd22e29f3695c2f15a7dce7ec1cfbcb9f
gagatpadi <padiyogaradhana@gmail.com>
2024-04-23 19:22:15 +0700

    void output

--- oop1/oop1.cpp
+++ oop1/oop1.cpp
@@ -7,4 +7,12 @@ public: // akses modifier
 	int umur;
 	string jurusan;
 
-	
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan: " << jurusan << endl;
+	}
+
+};
+
+
