8929862fb8c82c4d67da3e8ede302ad8d6e6bf2f
gagatpadi <padiyogaradhana@gmail.com>
2024-04-23 18:50:39 +0700

    class matakuliah (private)

--- oop1/oop1.cpp
+++ oop1/oop1.cpp
@@ -12,4 +12,13 @@ public: // akses modifier
 		cout << "Umur: " << umur << endl;
 		cout << "Jurusan: " << jurusan << endl;
 	}
+
+};
+
+
+class Matakuliah {
+private:
+	string kodeMk;
+	string namaMk;
+	int sks;
 };
