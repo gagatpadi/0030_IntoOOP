5d8a93aa04cc64e94b3b7642427c4bb94addde6e
gagatpadi <padiyogaradhana@gmail.com>
2024-04-23 19:23:36 +0700

    private float public float

--- oop1/oop2.cpp
+++ oop1/oop2.cpp
@@ -9,30 +9,4 @@ private:
 public:
 	float luas;
 
-	void input() { // methode input persegi panjang
-
-		cout << "Masukan Panjangnya = ";
-		cin >> panjang;
-		cout << "Masukan Lebarnya = ";
-		cin >> lebar;
-	}
-
-	float hitungLuas() {
-		return panjang * lebar;
-	}
-
-	void display() {
-		cout << "Panjangnya = " << panjang << endl;
-		cout << "Lebarnya = " << lebar << endl;
-		cout << "Luasnya = " << hitungLuas() << endl;
-	}
-};
-
-int main() {
-	bangunDatar pp; // deklarasi objek pp dari class bangunDatar
-	pp.input();
-	pp.display();
-
-	return 0;
-}
-
+	
