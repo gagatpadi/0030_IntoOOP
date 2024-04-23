05e6850d97bfa47cce748fe4dffde86ed9b07c22
gagatpadi <padiyogaradhana@gmail.com>
2024-04-23 19:22:50 +0700

    int main mk mhs

--- oop1/oop1.cpp
+++ oop1/oop1.cpp
@@ -16,3 +16,45 @@ public: // akses modifier
 };
 
 
+class Matakuliah {
+private:
+	string kodeMk;
+	string namaMk;
+	int sks;
+public:
+	void input() {
+		cout << "Kode MK: ";
+		cin >> kodeMk;
+		cout << "Nama Mk";
+		cin >> namaMk;
+		cout << "SKS: ";
+		cin >> sks;
+	}
+
+	void output() {
+		cout << "Kode MK: " << kodeMk << endl;
+		cout << "Nama MK: " << namaMk << endl;
+		cout << "SKS: " << sks << endl;
+
+	}
+
+};
+
+int main() {
+	Mahasiswa mhs;
+	Matakuliah mk;
+
+	mhs.nama = "Budi";
+	mhs.umur = 20;
+	mhs.jurusan = "Teknik Informatika";
+	mhs.output();
+
+	mk.input();
+	mk.output();
+
+	
+
+}
+
+
+	
